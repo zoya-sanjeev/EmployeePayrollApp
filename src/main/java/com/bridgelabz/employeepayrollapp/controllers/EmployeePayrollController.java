26451341b34ca3130a24e0a200ff8aa21fbb07cf
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-02 20:23:27 +0530

    ADDED: EmployeePayrollDTO imported in controller class

--- src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
