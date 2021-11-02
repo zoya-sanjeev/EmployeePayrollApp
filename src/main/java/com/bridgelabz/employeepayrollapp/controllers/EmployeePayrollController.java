0a9a6b9c72a406735c5a77ff4b48118927afc84b
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-02 20:12:10 +0530

    ADDED: various http methods added to controller class

--- src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -2,6 +2,12 @@ package com.bridgelabz.employeepayrollapp.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +19,30 @@ public class EmployeePayrollController {
 	public ResponseEntity<String> getEmployeePayrollData(){
 		return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
 	}
+	
+	@GetMapping("/get/{empId}")
+	public ResponseEntity<String> getEmployeePayrollData(@PathVariable("empId") int empId){
+		return new ResponseEntity<String>("Get Call Success for id: " + empId, HttpStatus.OK);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<String> addEmployeePayrollData(
+				@RequestBody EmployeePayrollDTO empPayrollDTO){
+		return new ResponseEntity<String>("Created Employee Payroll Data for: "
+				+ empPayrollDTO, HttpStatus.OK);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<String> updateEmployeePayrollData(
+			@RequestBody EmployeePayrollDTO empPayrollDTO){
+		return new ResponseEntity<String>("Updated Employee Payroll Data for: "
+				+ empPayrollDTO, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/delete/{empId}")
+	public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("empId") int empId){
+		return new ResponseEntity<String>("Delete Call Success for id: " + empId, HttpStatus.OK);
+	}
+	
 
 }
