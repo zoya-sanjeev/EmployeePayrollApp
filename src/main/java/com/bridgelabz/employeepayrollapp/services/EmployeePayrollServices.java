0aa4eef0cccba58e8dff305fe5a8f5249f057fda
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-02 22:17:37 +0530

    ADDD: created class EmployeePayrollServices

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/EmployeePayrollServices.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.employeepayrollapp.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+
+public class EmployeePayrollServices implements IEmployeePayrollService{
+
+	@Override
+	public List<EmployeePayrollData> getEmployeePayrollData() {
+		List<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+		employeePayrollList.add(new EmployeePayrollData(1, new EmployeePayrollDTO("Pankaj", 3000)));
+		return employeePayrollList;
+	}
+
+	@Override
+	public EmployeePayrollData getEmployeePayrollDataById(int empId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO empPayrollDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteEmployeePayrollData(int empId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
