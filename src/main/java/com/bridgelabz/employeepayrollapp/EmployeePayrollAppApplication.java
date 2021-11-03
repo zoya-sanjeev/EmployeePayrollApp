dfd0a19d1dc0c5d3eecd18b38450e561b8ae80e4
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-04 01:17:17 +0530

    ADDED: environment for employee payroll application

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -2,12 +2,17 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		ApplicationContext context = SpringApplication
+				.run(EmployeePayrollAppApplication.class, args);
 	}
 
 }
