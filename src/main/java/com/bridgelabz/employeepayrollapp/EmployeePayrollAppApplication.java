cd0264984e618ca8e7cf58efc482a298cef41758
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-04 01:21:20 +0530

    ADDED: used logger to display current environment

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -13,6 +13,8 @@ public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication
 				.run(EmployeePayrollAppApplication.class, args);
+	log.info("Employee Payroll App Started in {} Environment", 
+			context.getEnvironment().getProperty("environment"));
 	}
 
 }
