558ed78a75bcac633cb809b9aef9c4f2c42d6654
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-04 15:45:02 +0530

    ADDED: function to handle exceptions

--- src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionsHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionsHandler.java
@@ -1,8 +1,30 @@
 package com.bridgelabz.employeepayrollapp.exceptions;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
 
 @ControllerAdvice
 public class EmployeePayrollExceptionsHandler {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMsg = errorList.stream()
+				.map(objErr -> objErr.getDefaultMessage())
+				.collect(Collectors.toList());
+		ResponseDTO response = new ResponseDTO("Exception while processing request", errMsg);
+		return new ResponseEntity<ResponseDTO>(response, HttpStatus.BAD_REQUEST);
+	}
+	
 
 }
