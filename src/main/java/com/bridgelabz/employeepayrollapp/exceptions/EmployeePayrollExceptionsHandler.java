cfdc1a79ce960de5448f6090c8c5a56247871a0c
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-04 15:40:15 +0530

    ADDED: EmployeePayrollExceptionHandler created

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionsHandler.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayrollapp.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class EmployeePayrollExceptionsHandler {
+
+}
