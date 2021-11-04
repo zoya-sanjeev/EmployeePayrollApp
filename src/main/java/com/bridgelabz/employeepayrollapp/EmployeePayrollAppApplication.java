e05314725dbbf949d9a7dad8ad1644ac94bbb96b
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-04 13:16:20 +0530

    ADDED: logger used to display username

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -15,6 +15,8 @@ public class EmployeePayrollAppApplication {
 				.run(EmployeePayrollAppApplication.class, args);
 	log.info("Employee Payroll App Started in {} Environment", 
 			context.getEnvironment().getProperty("environment"));
+	log.info("Emploee Payroll DB User is {}", 
+			context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
