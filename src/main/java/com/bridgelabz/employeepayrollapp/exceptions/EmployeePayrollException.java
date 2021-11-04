fb54ef726aadba87ba694863ea3eb9f7382a1d5d
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-04 15:46:55 +0530

    ADDED: created custom exception class

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayrollapp.exceptions;
+
+public class EmployeePayrollException extends RuntimeException{
+	public EmployeePayrollException(String message) {
+		super(message);
+	}
+
+}
