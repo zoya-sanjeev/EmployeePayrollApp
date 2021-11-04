c3bb5b0d2f51f84d80949028f1cc4aaf82f12e11
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-04 15:37:49 +0530

    ADDED: validation for salary

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,12 +1,13 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 
 public class EmployeePayrollDTO {
 	@Pattern(regexp = "^[A-Z] {1}[a-zA-Z\\s] {2,}$", message = "Employee name is invalid")
 	public String name;
 	
-	
+	@Min(value = 500, message = "Min Wage should be more than 500")
 	public long salary;
 	
 	public EmployeePayrollDTO(String name, long salary) {
