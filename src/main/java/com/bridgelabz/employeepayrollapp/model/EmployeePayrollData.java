80a4b63ec2445713d659e21790d1bf4c8c66ea01
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-06 23:06:48 +0530

    ADDED: created EmployeePayrollRepository

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -3,18 +3,40 @@ package com.bridgelabz.employeepayrollapp.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name = "employee_payroll")
 public @Data class EmployeePayrollData {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "employee_id")
 	private int employeeId;
+	
+	@Column(name = "name")
 	private String name;
 	private long salary;
 	private String gender;
 	private LocalDate starteDate;
 	private String note;
 	private String profilePic;
+	
+	@ElementCollection
+	@CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "department")
 	private List<String> department;
 	
 	public EmployeePayrollData() {
