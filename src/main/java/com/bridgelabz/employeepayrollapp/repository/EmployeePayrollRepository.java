413d5775cb4c663ea95a634306ccd256b7587f41
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-06 23:34:33 +0530

    ADDED: custom query added

--- src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.employeepayrollapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 
 public interface EmployeePayrollRepository  extends JpaRepository<EmployeePayrollData, Integer>{
-
-}
+	@Query(value="select * from employee_payroll, employee_department where employee_id = id and department = :department", nativeQuery = true)
+	List<EmployeePayrollData> findEmployeesByDepartment(String department);
+} 
