861ffdef2f1ea63591d3ffc036ffbaac5a27c20c
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-06 22:34:27 +0530

    ADDED: validation for gender, note, dept, profilepic, date

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -17,15 +20,21 @@ public @ToString class EmployeePayrollDTO {
 	@Min(value = 500, message = "Min Wage should be more than 500")
 	public long salary;
 	
+	@Pattern(regexp = "male|female", message ="Gender needs to be male or female")
 	public String gender;
 	
 	@JsonFormat(pattern="dd MMM yyyy")
+	@NotNull(message = "startDate should Not be empty")
+	@PastOrPresent (message = "startDate should be past or today's date")
 	public LocalDate startDate;
 	
+	@NotBlank(message = "Note cannot be Empty")
 	public String note;
 	
+	@NotBlank(message = "profilePic cannot be Empty")
 	public String profilePic;
 	
+	@NotNull(message = "department should not be empty")
 	public List<String> department;
 	
 }
